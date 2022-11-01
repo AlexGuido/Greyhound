0e9064b957886fdea1c163134e644808d6349b00
AlexGuido <alexanderguido@outlook.com>
2022-11-01 13:46:56 -0600

    Variables en python con funcion input

---
+++ python/Basics/2.py
@@ -0,0 +1,16 @@
+print("Hola, vars")
+
+#esto es una variable 
+my_name = "Alexander"
+print(my_name)
+
+# esto es una variable con un numero
+my_age = 12
+print(my_age)
+
+my_name = "Mateo"
+print("Aqui cambio", my_name)
+
+# input 
+my_name = input("¿Cual es tu nombre? ")
+print("Usando input", my_name)
