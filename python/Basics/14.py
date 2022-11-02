254ed88f2229bf56a99df0daf16444a4a97b3ca6
alexguido <alexanderguido@outlook.com>
2022-11-01 23:59:28 -0600

    indexing y slicing

---
+++ python/Basics/14.py
@@ -0,0 +1,17 @@
+#indexing y slicing
+text = 'Ella sabe Python'
+print(text[0])
+print(text[1])
+size = len(text)
+print('size =>', size)
+print(text[size -1])
+print(text[-1])
+
+#slicing
+print(text[0:5])
+print(text[10:16])
+print(text[:10])
+print(text[5:])
+print(text[:])
+print(text[10:16:2])
+print(text[::2])
