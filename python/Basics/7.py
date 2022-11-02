aa6a368cf7d9bc93bfe3a337363b5c653c2f3afe
alexguido <alexanderguido@outlook.com>
2022-11-01 18:12:29 -0600

    operadores aritmeticos

---
+++ python/Basics/7.py
@@ -0,0 +1,13 @@
+#operadores aritmeticos
+
+print(10 + 10)
+print(10 - 5)
+print(10 *2)
+print(10/2)
+print(10%2)
+print("Hola" + "Hola")
+print("Hola" * 3)
+
+print(10 // 3)
+print(2**3)
+print(2 ** 3+3-7/1 //4)
