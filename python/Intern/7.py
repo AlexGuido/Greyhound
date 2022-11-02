1950bef85a188e527ae81152a9bd3ffd00040d72
alexguido <alexanderguido@outlook.com>
2022-11-02 15:28:53 -0600

    Funciones

---
+++ python/Intern/7.py
@@ -0,0 +1,17 @@
+#funciones
+print('Hola')
+
+def myPrint(text):
+    print(text * 2)
+    
+myPrint('Este es mi texto')
+myPrint('Hola')
+
+a = 10
+b = 90
+c = a + b
+
+def suma(a,b):
+    myPrint(a + b)
+suma(1,5)
+suma(10,4)
