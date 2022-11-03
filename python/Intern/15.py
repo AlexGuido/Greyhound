c4d9f2ac3a1f8decef9c2cb729394b21475483e3
alexguido <alexanderguido@outlook.com>
2022-11-03 12:37:41 -0600

    map con inmutabilidad

---
+++ python/Intern/15.py
@@ -0,0 +1,27 @@
+# map con inmutabilidad
+
+items = [
+    {
+        'product':'camisa',
+        'price':100
+    },          
+    {
+        'product': 'pantalones',
+        'price':300,
+    },
+    {
+        'product': 'pantalones 2',
+        'price': 200
+    }
+]
+
+prices = list(map(lambda item: item['price'],items))
+print(prices)
+
+def addTaxes(item):
+    newItem = item.copy()
+    item['taxes'] = item['price']*0.16
+    return item
+
+newItems = list(map(addTaxes,items))
+print(newItems) 
