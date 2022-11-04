84fafbb540e390c39152e91c7ca8ceb5211f0bc3
alexguido <alexanderguido@outlook.com>
2022-11-04 14:56:37 -0600

    paquetes

---
+++ python/Intern/main.py
@@ -0,0 +1,15 @@
+'''
+from pkg.mod1 import func1, func2
+from pkg.mod2 import func3, func4
+
+
+print(func1())
+print(func2())
+
+print(func3())
+print(func4())
+'''
+
+import pkg
+print(pkg.url)
+print(pkg.mod1.func1())
---
+++ python/Intern/pkg/__init__.py
@@ -0,0 +1,5 @@
+print('Se inicio paquete')
+
+url = 'alexguido.com'
+
+import pkg.mod1, pkg.mod2
